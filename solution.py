7e6365bf4a74bfa7dd64782aa876def05ca3cf0f
San4ezz28 <101478251+San4ezz28@users.noreply.github.com>
2023-04-06 17:52:11 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,11 +1,13 @@
 import pandas as pd
 import numpy as np
+import math
+from scipy.stats import norm
 
 
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
+chat_id = 5960781343                    # Ваш chat ID, не меняйте название переменной
 
 def solution(x: np.array) -> float:
-    # Измените код этой функции
-    # Это будет вашим решением
-    # Не меняйте название функции и её аргументы
-    return x.mean() # Ваш ответ
+    mu, sigma = norm.fit(x)
+    a = 2/(np.exp(2)-1)
+    a_cf = a*mu/10
+    return a_cf
