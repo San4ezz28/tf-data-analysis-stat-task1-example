d548fd7f1ed95177c7bc6bd5f5ba518c4318a1fd
San4ezz28 <101478251+San4ezz28@users.noreply.github.com>
2023-04-06 13:41:40 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,11 +1,20 @@
 import pandas as pd
 import numpy as np
+import math
 
 
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
+
+chat_id = 5960781343                    # Ваш chat ID, не меняйте название переменной
 
 def solution(x: np.array) -> float:
-    # Измените код этой функции
-    # Это будет вашим решением
-    # Не меняйте название функции и её аргументы
-    return x.mean() # Ваш ответ
+   # Рассчитаем среднее значение и дисперсию измерений скорости
+   x_1 = np.array(x)
+   mean_speed = x_1.mean()
+   disper = x_1.var()
+       # Рассчитаем точечную оценку коэффициента ускорения
+   norm_raspred =  1/(disper*(2*math.pi)**0.5) * math.exp(-((-1-math.exp(1) - mean_speed)**2)/2*disper**2) # Формула из постановки задачи
+   a = norm_raspred/10
+   return a,disper # Ваш ответ
+
+x = [10, 15 , 17 , 20 ,12 ,14 ]
+print(solution(x))
